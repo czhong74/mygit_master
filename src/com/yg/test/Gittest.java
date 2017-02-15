caefe408da533f169440839ecc1ddb098e38c261
zhong74 <zhong74@sina.com>
2017-02-15 13:50:39 +0800

    初始化项目

--- src/com/yg/test/Gittest.java
+++ src/com/yg/test/Gittest.java
@@ -7,5 +7,6 @@ public class Gittest {
     public static void main(String[] args){
     System.out.println("dddd");
         System.out.println("dddd");
+        System.out.println("dddd");
     }
 }
